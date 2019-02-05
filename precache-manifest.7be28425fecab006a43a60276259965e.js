9d8d41ca88722700eef191c19548562897b272e5
Anna Kowalczuk <anna@kowalczuk.eu>
2019-02-05 23:23:39 +0100

    Updates

---
+++ precache-manifest.7be28425fecab006a43a60276259965e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "427d2c5b07207fb08693",
+    "url": "/React-stopwatch-19.7/static/js/runtime~main.427d2c5b.js"
+  },
+  {
+    "revision": "3d6b0f1deb1e8bc50204",
+    "url": "/React-stopwatch-19.7/static/js/main.3d6b0f1d.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/React-stopwatch-19.7/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "3d6b0f1deb1e8bc50204",
+    "url": "/React-stopwatch-19.7/static/css/main.33cf1500.chunk.css"
+  },
+  {
+    "revision": "af5b5c4d122772546ac02beace4061e3",
+    "url": "/React-stopwatch-19.7/index.html"
+  }
+];
